3991ce278273d86a51b99b10ba0788c41b23794c
febenitez04 <febenitez@frba.utn.edu.ar>
2024-09-24 16:48:33 -0300

    Feature Mensaje Ser

--- ruby/lib/tadspec/tad_spec.rb
+++ ruby/lib/tadspec/tad_spec.rb
@@ -27,8 +27,18 @@ module Criteria
     Config.new(proc {|object| object < algo})
   end
 
+  def ser(config)
+    #Si no es un config, comparo por igual
+    unless config.is_a? Config
+      igual_a(config)
+    else
+      config #Se ejectua el proc
+    end
+  end
+
 end
 
+#Abro la clase Object para inyectarle el mensaje deberia
 class Object
   def deberia(criteria)
     criteria.call(self)
