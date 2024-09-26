306ce7292997d8a22eff0f4bb4d7ee4b8d23a90d
febenitez04 <febenitez@frba.utn.edu.ar>
2024-09-26 12:55:40 -0300

    Test ser implementa personas

--- ruby/spec/test_ser_spec.rb
+++ ruby/spec/test_ser_spec.rb
@@ -47,6 +47,27 @@ describe "mensaje_ser" do
     }.to raise_error(TadspecAssertionError)
   end
 
+  #Test con Clase Persona
+  let(:persona_joven) { Persona.new('Pepe', 25) }
+  let(:persona_vieja) { Persona.new('Pancracio', 35) }
+
+  it 'Persona vieja deberia ser vieja' do
+    expect(persona_vieja.viejo?.deberia ser true).to be(true)
+  end
+
+  it 'Persona Joven no deberia ser vieja' do
+    expect(persona_joven.viejo?.deberia ser false).to be(true)
+  end
+
+  it 'Persona joven deberia llamarse Pepe y tiene 25 años' do
+    expect(persona_joven.nombre.deberia ser 'Pepe').to be(true)
+    expect(persona_joven.edad.deberia ser 25).to be(true)
+  end
+
+  it 'Persona vieja deberia llamarse Pancracio' do
+    expect(persona_vieja.nombre.deberia ser 'Pancracio').to be(true)
+    expect(persona_vieja.edad.deberia ser 35).to be(true)
+  end
 end
 
 
