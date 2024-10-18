72584fed03d1aad208c8d22f9e2bd7474331994b
febenitez04 <febenitez@frba.utn.edu.ar>
2024-10-18 13:22:14 -0300

    tests actualizados

--- ruby/spec/spec_helper.rb
+++ ruby/spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require 'rspec'
 
 require_relative '../lib/tadspec/tad_spec'
+require_relative '../lib/tadspec/criteria'
 require_relative '../lib/persona'
 require_relative '../lib/suites/mi_suite'
